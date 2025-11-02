71bb94ec8a012e971f000327c4f95f00fcdf1977
Asephera <sonofgeek@hotmail.com>
2025-11-02 01:25:40 -0700

    player level tweaks and death

--- Player/player.gd
+++ Player/player.gd
@@ -3,11 +3,12 @@ class_name player
 
 ## On READYS
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var camera_2d: Camera2D = $Camera2D
 
 # EXPORTS
 @export var sprite_texture : Texture
 @export var speed_multiplier := 1
-@export_range(0,4) var channel : int = 2 ## set the starting channel from 0 to 4
+@export_range(1,5) var channel : int = 3 ## set the starting channel from 0 to 4
 
 func _ready() -> void:
 	add_to_group("players")
@@ -16,18 +17,31 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	velocity.x = get_gravity().y * _delta * speed_multiplier
+	print(velocity)
 	
-	if channel:
-		_set_y_pos(channel)
+	if channel: _set_y_pos(channel)
+	
+	move_and_slide()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("up"):
-		if channel != 4: channel += 1
+		if channel != 1: channel -= 1
 		else: pass # NOTE: can add error sound effect or som here and flash sprite red or som
 	if event.is_action_pressed("down"):
-		if channel != 0: channel -= 1
+		if channel != 5: channel += 1
 		else: pass # NOTE: can add error sound effect or som here and flash sprite red or som
 
 func _set_y_pos(height):
-	position.y = height * 8 - 8 * 4 # 8 pixels per channel, might need tweaking
+	position.y = height * 8 - 8 * 5 # 8 pixels per channel, might need tweaking
 	# NOTE: Can add vertical shake when moving here
+
+
+func _on_collision_area_body_entered(body: Node2D) -> void:
+	if body.name == "Spikes":
+		print("die")
+		camera_2d.reparent(get_tree().current_scene)
+		queue_free()
+		
+		await get_tree().create_timer(1).timeout
+	if body.name == "Victory":
+		print("win")
