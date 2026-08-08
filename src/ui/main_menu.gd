03a8297688256faa32faaae00b0951338da0a965
Ashli <87504872+Lukkex@users.noreply.github.com>
2026-08-07 19:30:05 -0700

    fade in from white animation for main menu screen

--- src/ui/main_menu.gd
+++ src/ui/main_menu.gd
@@ -1,7 +1,9 @@
 extends Control
 
 @onready var music = $SnordMusicLoop
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var video_stream_player: VideoStreamPlayer = $MarginContainer2/VBoxContainer/VideoStreamPlayer
 
 func _ready() -> void:
 	#music.play()
-	pass
+	animation_player.play("fade_in")
