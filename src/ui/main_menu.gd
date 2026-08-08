fa69b8829f72014ad03c7a0dbbfd429d12e722cd
Ashli <87504872+Lukkex@users.noreply.github.com>
2026-08-07 19:35:14 -0700

    removes colorrect automatically from main menu after fade in animation finishes

--- src/ui/main_menu.gd
+++ src/ui/main_menu.gd
@@ -3,7 +3,10 @@ extends Control
 @onready var music = $SnordMusicLoop
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var video_stream_player: VideoStreamPlayer = $MarginContainer2/VBoxContainer/VideoStreamPlayer
+@onready var color_rect: ColorRect = $ColorRect
 
 func _ready() -> void:
 	#music.play()
 	animation_player.play("fade_in")
+	await animation_player.animation_finished
+	remove_child(color_rect)
