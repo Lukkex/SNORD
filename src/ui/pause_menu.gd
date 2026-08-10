1befdde54d7fcbf020de53cb9891de54a375d3b8
Asephera <Asephera@outlook.com>
2026-08-10 01:38:04 -0700

    funny pause menu
    - pausing zooms on character
    - reorganized menu
    - display name

--- src/ui/pause_menu.gd
+++ src/ui/pause_menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var quiz_manager : Control = $"../../QuizManager"
 @onready var settings_menu: Control = $Settings
+@onready var character_name: Label = $MarginContainer/HBoxContainer/CharacterName
 
 @onready var player : CharacterBody3D
 
@@ -11,16 +12,28 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("escape"):
+		
+		character_name.text = Global.character.character_name
 		settings_menu.visible = false
 		pausemenu()
 
 func pausemenu():
 	if visible:
+		var tween = create_tween()
+		tween.tween_property( Global.player_camera, "zoom", Vector2(10, 10), 0.1 )
+		tween.parallel().tween_property( Global.player_camera, "offset", Vector2(0, 0), 0.15 )
 		visible = false
+		
+		await tween.finished
 		get_tree().paused = false
 	else:
-		visible = true
+		var tween2 = create_tween()
+		tween2.tween_property( Global.player_camera, "zoom", Vector2(100, 160), 0.1 )
+		tween2.parallel().tween_property( Global.player_camera, "offset", Vector2(6, 0), 0.15 )
 		get_tree().paused = true
+		
+		await tween2.finished
+		visible = true
 
 func _on_resume_pressed():
 	if visible:
