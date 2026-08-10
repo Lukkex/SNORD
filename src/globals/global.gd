e6463a3e85ffa5d35e6c128cb441469e8148abd9
Ashli <87504872+Lukkex@users.noreply.github.com>
2026-08-09 20:36:07 -0700

    HUD added, speedrun timer functional!

--- src/globals/global.gd
+++ src/globals/global.gd
@@ -2,7 +2,8 @@ extends Node
 
 @onready var DEFAULT_SKIN = preload("res://assets/snord.png")
 @onready var player_skin = DEFAULT_SKIN
+
 var speed_multiplier : float = 10.0
 var player : Player
-
+var timer_enabled : bool = true
 var character : CharProfile = preload("uid://c1m6f6h85nuno")
