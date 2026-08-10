1847f0d7adf64f6ffcd30c32085962d701a7743a
Akai <87504872+Lukkex@users.noreply.github.com>
2026-08-10 00:26:33 +0000

    Hotfixes (#13)
    * saved snordlings to resource files
    
    * Default character set to snord, no more crash if you bypass player select screen w debug menu
    
    * renamed snort to snord lol

--- src/globals/global.gd
+++ src/globals/global.gd
@@ -2,8 +2,7 @@ extends Node
 
 @onready var DEFAULT_SKIN = preload("res://assets/snord.png")
 @onready var player_skin = DEFAULT_SKIN
-
 var speed_multiplier : float = 10.0
 var player : Player
 
-var character : CharProfile
+var character : CharProfile = preload("uid://c1m6f6h85nuno")
