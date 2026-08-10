351ae35a27d6e34549acc8951a23f3576dec716f
Ashli <87504872+Lukkex@users.noreply.github.com>
2026-08-10 01:46:27 -0700

    WIP character sounds - dictionary array in audiomanager next, or a diff way to handle sounds

--- src/managers/audio_manager/audio_manager.gd
+++ src/managers/audio_manager/audio_manager.gd
@@ -1,8 +1,23 @@
 extends Node2D
 
+@export_file_path() var character_sound_directory : String
+
 @onready var snordmusicloop = $SnordMusicLoop
 @onready var snord1 = $SnordSound1
+
+func _ready() -> void:
+	load_character_sounds(character_sound_directory)
 
 func stop_all_sounds() -> void:
 	for sound in get_children():
 		sound.stop()
+
+func load_character_sounds(path):
+	var dir = DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			# do something with the file
+			print(file_name)
+			file_name = dir.get_next()
