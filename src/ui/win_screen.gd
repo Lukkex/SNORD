17ba9c973515f481594530a516b3d02f25828e79
Ashli <87504872+Lukkex@users.noreply.github.com>
2026-08-09 21:33:37 -0700

    "next level" button is hidden now after completing the final level of the game

--- src/ui/win_screen.gd
+++ src/ui/win_screen.gd
@@ -1,6 +1,14 @@
 extends Control 
 
+@onready var next_level_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/NextLevelButton
+
 var scene : PackedScene
+
+func _ready() -> void:
+	next_level_button.visible = true
+	
+	if LevelManager.is_on_last_level():
+		next_level_button.visible = false
 
 func _on_next_level_button_pressed() -> void:
 	LevelManager.swap_scene_to_next_level()
