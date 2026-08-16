c944dd30f60b695e2a20de88ec5164f47f8ca60b
Asephera <Asephera@outlook.com>
2026-08-15 17:21:29 -0700

    the cells break (peak)
    - cells kill themselves when kyle touches them (just like me fr)
    - integration for literally anythign to happen when he touches them
    - currently just kills all tiles that arent on the background layer, can be changed later.
    - code is in the tilemap super node script.
    
    - todo: add particle effects when they die
    - export vars to modify code.

--- src/levels/Functions/tile_break_area.gd
+++ src/levels/Functions/tile_break_area.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 var tile : Vector2i
 var tile_region : Vector2i
+var layer : TileMapLayer
 
-func on_tile_entered():
-	print("tile entered!")
+func _on_area_entered(_area: Area2D) -> void:
+	SignalBus.iamatileandyoushouldkillmethanks.emit(tile, layer)
--- src/levels/tilemap.gd
+++ src/levels/tilemap.gd
@@ -1,35 +1,22 @@
 extends Node2D
 
 @onready var walls: TileMapLayer = $Walls
+@onready var spikes: TileMapLayer = $Spikes
 const TILE_BREAK_AREA = preload("uid://bdf36hjufx0bj")
 
-func break_tile(collision_pos: Vector2):
-	var cell_position = walls.local_to_map(collision_pos)
-	walls.erase_cell(cell_position)
-
 func _ready() -> void:
-	print("testing\n\nTiles!")
-	
-	"Returns all cells with the given tile id"
-	var tiles = walls.get_used_cells_by_id(0)
-	print(tiles, "\n\nTile Global Positions!")
-	
-	"Returns global position of every tile lol"
-	print( walls.map_to_local(tiles[0]) , "\n\n coordinate to tile itself..?")
-	
-	"Returns coordinate of tile in the tileset atlas"
-	print( walls.get_cell_atlas_coords( tiles[0] ) )
-	
-	
-	
-	for tile in tiles:
-		if walls.get_cell_atlas_coords( Vector2i(tile[0],tile[1]) ) == Vector2i(3,0):
-			var tile_break_area_instance = TILE_BREAK_AREA.instantiate()
-			tile_break_area_instance.position = walls.map_to_local(tile)
-			tile_break_area_instance.tile = tile
-			tile_break_area_instance.tile_region = walls.get_cell_atlas_coords( Vector2i(tile[0], tile[1]) )
-			walls.add_child(tile_break_area_instance)
-			tile_break_area_instance.connect("area_entered", on_tile_entered)
+	for map_layer in get_children():
+		if map_layer is TileMapLayer and map_layer.name != "Background":
+			var tile_array : Array[Vector2i] = map_layer.get_used_cells()
+			
+			for tile in tile_array:
+				var tile_break_area_instance = TILE_BREAK_AREA.instantiate()
+				tile_break_area_instance.position = map_layer.map_to_local(tile)
+				tile_break_area_instance.tile = tile
+				tile_break_area_instance.tile_region = map_layer.get_cell_atlas_coords( Vector2i(tile[0], tile[1]) )
+				tile_break_area_instance.layer = map_layer
+				map_layer.add_child(tile_break_area_instance)
+				SignalBus.iamatileandyoushouldkillmethanks.connect(tile_detected)
 
-func on_tile_entered(body = null):
-	print("tile entered!")
+func tile_detected(tile_import, layer_import):
+	layer_import.erase_cell(tile_import)
